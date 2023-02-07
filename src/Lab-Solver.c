eb0996a3c6575bbfe26bd7bfb57f0869555efab8
guillerubio <92153477+guillerubio@users.noreply.github.com>
2023-02-07 18:48:12 +0100

    Progress in string -> matrix

--- src/Lab-Solver.c
+++ src/Lab-Solver.c
@@ -8,7 +8,7 @@
 struct Maze {
     int rows;
     int columns;
-    // char** maze;
+    char** matrix;
 };
 
 // Maze constructor, a function that returns a pointer to a new maze
@@ -21,7 +21,7 @@ struct Maze* Maze(char* maze_string){
     while(*aux) { // Count columns and rows
         if (!doneColumns && *aux != '\n') {
             lColumns ++;
-        } else if (*aux == '\n' || *aux == '/0'){
+        } else if (*aux == '\n'){
             doneColumns = 1;
             lRows ++;
         }
@@ -31,6 +31,24 @@ struct Maze* Maze(char* maze_string){
     struct Maze *m = (struct Maze*) malloc(sizeof(struct Maze));
     (*m).rows = lRows;
     (*m).columns = lColumns;
+    // Memory allocation for maze matrix
+    char ** matrix = (char **) malloc((*m).columns * sizeof(char*));
+    for (int i=0; i < lRows; i++)
+        matrix[i] = (char**) malloc((*m).columns * sizeof(char));
+    // From string to matrix huzza!
+/*    aux = maze_string; // Auxiliary pointer reset
+    int i = 0; // row iteration variable
+    int j = 0; // column iteration variable
+    while(aux) {
+        if (*aux != '/n') {
+            matrix[i][j] = *aux;
+            j++;
+        } else {
+            i++;
+            j=0;
+        }
+        aux++;
+    }*/
 
     return m;
 
